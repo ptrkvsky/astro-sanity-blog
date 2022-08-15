e90fe7c63121b4dd384df7656e561bb65c4d12ed
Johan Petrikovsky <johan.petrikovsky@new-immo-group.com>
2022-08-15 21:19:35 +0200

    feat: :sparkles: first commit

---
+++ src/helpers/getToc.ts
@@ -0,0 +1,8 @@
+import type { TableOfContent } from '@interfaces/TableOfContent';
+
+/**
+ *
+ * @param headings - Array of headings from markdown file
+ * @returns HTML ordered list of headings
+ */
+const getTableOfContents = (headings: TableOfContent[]) => {};

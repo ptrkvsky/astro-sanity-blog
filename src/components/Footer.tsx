5bb0d9dd2bed7292391f0fc0b711c0b22d933af5
johan petrikovsky <johan.petrikovsky@gmail.com>
2022-09-11 16:15:44 +0200

    feat: :sparkles: add legal mentions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <footer className="py-12 bg-black text-white">
       <div className="pt-4 pb-3 mx-4 md:mx-auto flex place-content-between max-w-6xl">
         <a href="/plan-site">Plan du site</a>
+        <a href="/mentions-legales">Mentions légales</a>
       </div>
-      {/* <a href="/mentions-legales">Mentions légales</a> */}
     </footer>
   );
 };

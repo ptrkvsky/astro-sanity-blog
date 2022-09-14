33559247439e835dad8c0e34f098c6847bc1f2c7
johan petrikovsky <johan.petrikovsky@gmail.com>
2022-09-14 17:02:07 +0200

    feat(posts): :sparkles: add comments

---
+++ src/features/blog/functions/getPostsComments.ts
@@ -0,0 +1,19 @@
+import { client } from '@lib/sanityClient';
+
+export default async function getPostComments(refPost: string) {
+  const query = `*[_type == "comment" && isActive === true && references($refPost)]`;
+  const params = {
+    refPost,
+  };
+
+  try {
+    const result = await client.fetch(query, params);
+    return result;
+  } catch (error: any) {
+    console.dir(error, { depth: null });
+    // console.error('👨‍🚒 error', error.body.error);
+    return {
+      error: error.res,
+    };
+  }
+}

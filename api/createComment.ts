763a0ec3ec5413c76dbac1215f2a30ef44d7c929
johan petrikovsky <johan.petrikovsky@gmail.com>
2022-09-15 20:59:23 +0200

    fix(global): :bug: update import env variables for the edge

--- api/createComment.ts
+++ api/createComment.ts
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 
 const config = {
   projectId:
-    import.meta?.env?.PUBLIC_SANITY_PROJECT_ID ||
-    process.env.PUBLIC_SANITY_PROJECT_ID,
+    process.env.PUBLIC_SANITY_PROJECT_ID ||
+    import.meta?.env?.PUBLIC_SANITY_PROJECT_ID,
   dataset:
-    import.meta?.env?.PUBLIC_SANITY_DATASET ||
-    process.env.PUBLIC_SANITY_DATASET,
+    process.env.PUBLIC_SANITY_DATASET ||
+    import.meta?.env?.PUBLIC_SANITY_DATASET,
   token: import.meta.env.SANITY_READ_TOKEN || process.env.SANITY_READ_TOKEN,
   apiVersion: '2021-03-25',
   useCdn: true,

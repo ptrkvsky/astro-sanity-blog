5d56d7601cfe524ad5a1ecc8a8173a92b9774463
johan petrikovsky <johan.petrikovsky@gmail.com>
2024-09-15 08:13:13 +0200

    feat: :sparkles: improve Header layout and update post layout with consistent heading styles across various components

--- src/features/blog/components/FormComment/FormComment.tsx
+++ src/features/blog/components/FormComment/FormComment.tsx
@@ -87,8 +87,8 @@ export default function FormComment({ postId, postTitle }: FormComment) {
   }
 
   return (
-    <section class="px-4 sm:px-0">
-      <h2 class="font-semibold text-indigo-600 mt-10 mb-4 text-2xl lg:text-3xl">
+    <section className="px-0 sm:pr-16">
+      <h2 className="font-semibold text-indigo-600 mt-10 mb-4 text-2xl lg:text-3xl">
         Laisser un commentaire
       </h2>
       <form onSubmit={handleSubmit}>

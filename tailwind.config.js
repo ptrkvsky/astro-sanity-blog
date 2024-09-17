87f7dfac4b9bf09d3c00e0eeb345f284ad081741
johan petrikovsky <johan.petrikovsky@gmail.com>
2024-09-17 22:36:00 +0200

    style: 🎨 improve typography and accessibility across multiple components with updated classes and aria attributes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,12 @@ module.exports = {
     fontFamily: {
       sans: ['Polysans', 'sans-serif'],
     },
+    extend: {
+      fontSize: {
+        '6xl': ['4rem', { lineHeight: '1.2' }], // 4rem correspond à la taille de text-6xl, modifie lineHeight ici
+        '4xl': ['2.25rem', { lineHeight: '1.3' }], // line-height personnalisé pour text-4xl
+        '3xl': ['1.875rem', { lineHeight: '1.4' }], // 4rem correspond à la taille de text-6xl, modifie lineHeight ici
+      },
+    },
   },
 };
